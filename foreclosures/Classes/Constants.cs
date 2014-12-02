a5bd2ab93527f74836f6b9d02442c00ee92d59d8
Chris Justman <chrisjustman@aol.com>
2014-12-02 00:42:51 -0600

    alot2

---
+++ foreclosures/Classes/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace foreclosures
+{
+    public static class Constants
+    {
+        public const string USER_AGENT = "BrewTownBot";
+    }
+}
